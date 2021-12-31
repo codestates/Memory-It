3e2765d65a2546a7727909be0fe90e89222da8ee
cjhmoves33 <cjhmoves33@gmail.com>
2021-12-31 09:11:32 +0900

    .

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,19 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function App() {
-  const [test, setTest] = useState('disconnection')
+  // const [test, setTest] = useState('disconnection')
 
-  useEffect(() => {
-    axios.get(process.env.REACT_APP_EC2, { withCredentials: true }).then((data) => {
-      console.log(data)
-      setTest(data.data)
-    })
-  }, [])
+  // useEffect(() => {
+  //   axios.get(process.env.REACT_APP_EC2, { withCredentials: true }).then((data) => {
+  //     console.log(data)
+  //     setTest(data.data)
+  //   })
+  // }, [])
 
   return (
     <>
       <div>hello </div>
-      <div>서버 연결:{test}</div>
+      {/* <div>서버 연결:{test}</div> */}
     </>
   )
 }
