42a70305b64578a0800c2e2ea78bcd55021774b4
dh <jres1007@gmail.com>
2022-01-04 10:51:03 +0900

    typeorm01

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata'
 import { createConnection } from 'typeorm'
 
-import { Users } from './entity/User'
+import { Users } from './entity/Users'
 
 createConnection()
   .then(async connection => {
