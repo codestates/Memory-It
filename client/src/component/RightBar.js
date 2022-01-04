5686724630613c85b91ec8375e4750d347a7ea39
rkems0122 <rkems0122@gmail.com>
2022-01-04 18:12:19 +0900

    DetailedPost 하드코딩

--- client/src/component/RightBar.js
+++ client/src/component/RightBar.js
@@ -22,23 +22,52 @@ const AddPost = styled.img`
   margin: 0px auto;
 `
 
-const DetailedPost = styled.div``
-const PicturePost = styled.img``
+const DetailedPost = styled.div`
+  text-align: center;
+`
+const PicturePost = styled.img`
+  width: 25vw;
+`
 const OrderOfPost = styled.div``
-const DetailedMood = styled.div``
-const DetailContent = styled.div``
+const DetailedMood = styled.div`
+  text-align: right;
+  margin-right: 1vw;
+`
+const DetailContent = styled.div`
+  margin: 1vh;
+  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+  font-weight: bold;
+`
+const HorizenLine = styled.hr`
+  width: 24vw;
+  border: none;
+  border: 1px solid #C4C4C4;
+`
+const MapLayer = styled.div``
+const DetailedPlace = styled.img``
 
 function RightBar () {
   return (
     <>
-      <DefaultRightBar>
+      {/* <DefaultRightBar>
         <br /><br /><br /><br /> 
         <Content1>어서와요</Content1>
         <Content2>오늘 하루는 어떤 색이었나요?</Content2>
         <FiveMood src={logo} />
         <br /><br />
         <AddPost src={addPost} />      
-      </DefaultRightBar>
+      </DefaultRightBar> */}
+      <DetailedPost>
+        <PicturePost src={dummydata[3].src} />
+        <OrderOfPost>1/4</OrderOfPost>
+        <DetailedMood>파랑색, 보라색</DetailedMood>
+        <DetailContent>올해로 26살. 이젠 그냥 아저씨인 걸까?</DetailContent>
+        <br /><br />
+        <HorizenLine />
+        <MapLayer>
+          여기에 지도가 나옴
+        </MapLayer>
+      </DetailedPost>
     </>
   )
 }
