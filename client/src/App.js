8eb79a3845d2fb62e4173e350a677df5bbb827cf
cjhmoves33 <cjhmoves33@gmail.com>
2022-01-05 17:30:39 +0900

    merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,17 @@
-<<<<<<< HEAD
-import React, { useEffect, useState } from "react"
-import MyPost from "./component/MyPost"
-import './global.css'
-
-=======
 import React, { useEffect, useState } from 'react'
 import MyPost from './component/MyPost'
->>>>>>> feature/connectionTest
+import './global.css'
 
 // 테스트용 컴포넌트
-// import ResponseTester from './servertest/multer'
+import ResponseTester from './servertest/multer'
 import CookieTester from './servertest/cookie'
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       {/* <MyPost /> */}
       <ResponseTester />
       {/* <Testercss /> */}
-=======
-      <MyPost />
-      {/* <CookieTester /> */}
->>>>>>> feature/connectionTest
     </>
   )
 }
