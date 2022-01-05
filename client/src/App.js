95d878dd444081726daa9ecb6d143895b16f9f46
hyeyoungkim <velvetredvelvet91@gmail.com>
2022-01-05 17:02:07 +0900

    [Client]게시물 작성 페이지 스타일링 진행중

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react"
 import MyPost from "./component/MyPost"
+import './global.css'
 
 
 // 테스트용 컴포넌트
@@ -8,8 +9,9 @@ import ResponseTester from "./servertest/tester"
 function App() {
   return (
     <>
-      <MyPost />
-      {/* <ResponseTester /> */}
+      {/* <MyPost /> */}
+      <ResponseTester />
+      {/* <Testercss /> */}
     </>
   )
 }
