8d42cee2fb7bd1f8ec95b890aa4c7934623a8643
rkems0122 <rkems0122@gmail.com>
2022-01-06 22:48:00 +0900

    feat: add NotFound.js and contact us of Sidebar.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import MyPost from './MyPost'
+import NotFound from './component/NotFound'
 import './global.css'
 
 // 테스트용 컴포넌트
@@ -10,8 +11,8 @@ import CookieTester from './servertest/cookie'
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<MyPost />} />
-    
+      <Route path='/' element={<MyPost />}></Route>
+      <Route path='*' element={<NotFound />} ></Route>
     </Routes>
   )
 }
