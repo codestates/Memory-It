dee94d1c1fc4b360d2d6a5404b3985c7b5e9413f
cjhmoves33 <cjhmoves33@gmail.com>
2022-01-07 17:30:52 +0900

    Fix: merge

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,10 @@ import React, { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import MyPost from './MyPost'
 import NotFound from './component/NotFound'
-// import Signup from './pages/Signup'
+import Signup from './pages/Signup'
 import Login from './pages/Login'
 import ModifyUserInfo from './component/rightbar/ModifyUserInfo'
 import DetailedPost from './component/rightbar/DetailedPost'
-import ResponseTester from './servertest/multer'
-import CookieTester from './servertest/cookie'
 import MapType from './component/MapType'
 import './global.css'
 
@@ -21,11 +19,9 @@ function App() {
     <Routes>
       <Route path="/" element={<MyPost />}></Route>
       <Route path="/login" element={<Login />}></Route>
-      {/* <Route path="/signup" element={<Signup />}></Route> */}
+      <Route path="/signup" element={<Signup />}></Route>
       <Route path="*" element={<NotFound />}></Route>
     </Routes>
-
-  // <ResponseTester />
   )
 }
 
