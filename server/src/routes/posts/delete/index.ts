5d7643f7795b4e3f02487c6675986772ccbe390c
dh <jres1007@gmail.com>
2022-01-07 18:31:20 +0900

    extractingaddressfromimages

--- server/src/routes/posts/delete/index.ts
+++ server/src/routes/posts/delete/index.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
 import { NOT_FOUND, POST_DELETED } from '../../../hardWord'
-import { getManager } from 'typeorm'
+import { getManager, createQueryBuilder } from 'typeorm'
 import { Posts } from '../../../entity/Posts'
 import { Post_emotion } from '../../../entity/Post_emotion'
 import { Images } from '../../../entity/Images'
@@ -15,10 +15,42 @@ export default {
     const entityManager = getManager()
     const post = await entityManager.findOne(Posts, postId)
     console.log('이거불러온 포스트임ㅎㅎ', post)
+    /*
+    const postImageFiles = await entityManager.find(Images, { post: postId })
+    console.log('삭제될이미지', postImageFiles)
 
-    const imageFileName = await entityManager.find(Images, { post: postId })
-    console.log('삭제될이미지', imageFileName)
+이런식으로 불러와짐...쓸수가 없네...
+ [
+  Images { id: 17, address: 'download (1)1641540763818.jpeg' },
+  Images { id: 18, address: 'images1641540763820.jpeg' }
+]
+*/
+
+    const imageFile = await entityManager.query(
+      `select * from images where postId=${postId}`
+    )
+    const addressList = []
+    const results = imageFile.map(ele => {
+      return addressList.push(ele.address)
+    })
+    console.log('#########', addressList)
+    // console.log('쿼리로불러온삭제될이미지', postImageFiles)
+
     // const imageStorage = fs.unlink()
+    // const imageFile = await entityManager
+    //   .createQueryBuilder()
+    //   .select('address')
+    //   .from(Images, 'image')
+    //   .where('image.post=:post', { post: postId })
+    //   .getMany()
+
+    //const result = await entityManager.save(imageFile)
+    console.log('EEEEEEE', imageFile)
+
+    // const postImageFileName = postImageFiles.map(file => {
+    //   const fileName = file.split(' ')[1]
+    //   return file
+    // })
 
     if (!post) {
       res.status(404).send(NOT_FOUND)
