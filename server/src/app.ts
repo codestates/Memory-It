c3bda3fcc4627343b057da718ea7f8feed5ff3fe
cjhmoves33 <cjhmoves33@gmail.com>
2022-01-11 09:53:21 +0900

    Merge: 1/11

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,25 +15,19 @@ import beforeLogin from './preProcess/beforeLogin'
 const PORT = 8081
 createConnection().then(async connection => {
   const app = express()
-
   app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
   app.use(cookieParser())
   app.use(express.json())
   app.use(express.urlencoded({ extended: true }))
   app.use(morgan('dev'))
 
-  // app.use(express.static(__dirname + '/images'))
+  app.use(express.static(__dirname + '/uploads'))
   // console.log('어디경로임?', __dirname)
   // 어디경로임? /home/dh/Desktop/34/final project/Memory-It/server/src
 
-  app.use(
-    express.static('/home/dh/Desktop/34/final project/Memory-It/server/dummy/uploads')
-  )
-
-  app.get('/static', (req, res) => {
-    // 3번 미들웨어
-    res.status(200).sendFile(__dirname + '/images/download1641800053750.jpeg')
-  })
+  // app.use(
+  //   express.static('/home/dh/Desktop/34/final project/Memory-It/server/uploads')
+  // )
 
   app.get('/', currentUser, beforeLogin)
   // app.post('/posts', currentUser, beforeLogin)
