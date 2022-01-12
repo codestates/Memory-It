bb579191fd6d816470eff50dad04d97933cddd1d
dh <jres1007@gmail.com>
2022-01-12 19:32:40 +0900

    dddd

--- client/src/component/article/DiaryType.js
+++ client/src/component/article/DiaryType.js
@@ -9,6 +9,7 @@ import greenMood from '../../static/greenMood.png'
 import redMood from '../../static/redMood.png'
 import blueMood from '../../static/blueMood.png'
 import violetMood from '../../static/violetMood.png'
+import { v4 } from 'uuid'
 
 const Posts = styled.div`
   @media only screen and (max-width: 965px) {
@@ -92,20 +93,21 @@ const DiaryType = () => {
     4: false,
   })
 
-  let data = []
+  const [data, setData] = useState([])
   useEffect(async () => {
-  await axios.get('http://localhost:8081/posts?type=diary&year=2022',{
-    withCredentials: true
-  })
-    .then(res => {
-      data = res.data.data
-    })
-  })
-  const inputData = () => {
-    if (!data.hasOwnProperty('src')) {
-      data = dummydata
-    }
-  }
+    await axios
+      .get('http://localhost:8081/posts?type=diary&year=2022', {
+        withCredentials: true,
+      })
+      .then(res => {
+        setData(res.data.data)
+      })
+  }, [])
+  // const inputData = () => {
+  //   if (!data.hasOwnProperty('src')) {
+  //     data = dummydata
+  //   }
+  // }
 
   const moods = picture => {
     let mood = []
@@ -130,18 +132,55 @@ const DiaryType = () => {
     return mood
   }
 
-
   return (
     <Posts>
-      {inputData()}
+      {/* {inputData()} */}
       {data.map(post => (
-
-
         <PictureWrapper
-          key={post.id}
+          key={v4()}
           onClick={() => {
             dispatch(changeImage(post))
             dispatch(detailedPostMode())
+            // 리덕스 스토리지에 posId 값저장
+            // 일반 변수와 state 차이???
+
+            /*
+            위 예제는 state를 만들 때 array 자료형을 집어넣었고,
+
+{} 중괄호를 이용해 원하는 곳에 state 데이터들을 바인딩했습니다.
+
+state도 만드는 과정이 귀찮아서 그렇지 용도는 그냥 변수랑 똑같습니다.
+
+그럼 변수 만들어 쓰면 되는거지 왜 굳이 state 만들어쓰냐고요?
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+변수 말고 state에 데이터 저장해서 쓰는 이유
+
+ 
+
+힘들게 state를 만들어 써야하는 이유는 이겁니다.
+
+"변수가 변경될 때 자동으로 관련된 HTML을 재렌더링되게 만들고 싶으면"
+
+그럴 때 변수 말고 state에 저장해서 데이터바인딩 하셔야합니다.
+
+
+리액트는 state가 수정이 일어나면
+
+state가 포함된 HTML을 자동으로 재렌더링 해줍니다.
+https://codingapple.com/unit/react-3-state-usestate-hook/
+
+
+            */
           }}
           onMouseEnter={() => {
             setIsHovers({ ...isHovers, [post.id]: true })
@@ -150,8 +189,7 @@ const DiaryType = () => {
             setIsHovers({ ...isHovers, [post.id]: false })
           }}
         >
-
-          <Picture imageSrc={post.src} />
+          <Picture imageSrc={post.images} />
         </PictureWrapper>
       ))}
     </Posts>
