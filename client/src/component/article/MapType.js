1e7e6d28a04aba3e4643f02a9223c7884d2a23f1
rkems0122 <rkems0122@gmail.com>
2022-01-18 08:49:29 +0900

    feat:maptype 클릭시 이벤트 삭제

--- client/src/component/article/MapType.js
+++ client/src/component/article/MapType.js
@@ -72,25 +72,9 @@ function MapType({post}) {
       })
       marker.setMap(map)
       marker.setDraggable(true)
-      kakao.maps.event.addListener(map, 'click', function (mouseEvent) {
-        // 클릭한 위도, 경도 정보를 가져옵니다
-        var latlng = mouseEvent.latLng
-
-        // 마커 위치를 클릭한 위치로 옮깁니다
-        marker.setPosition(latlng)
-
-        dispatch(
-          postingmapMode(
-            { ...data, lat: latlng.getLat(), lng: latlng.getLng() },
-            postingImages
-          )
-        )
-      })
     }
 
     getMarkerImage()
-
-    // console.log('위치정보 변경되는지', data)
 
     var mapTypeControl = new kakao.maps.MapTypeControl()
 
