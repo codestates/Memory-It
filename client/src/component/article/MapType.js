47e58f9cfc9811bfeb568c9bfb42d19aeb6d7dd1
rkems0122 <rkems0122@gmail.com>
2022-01-18 10:21:14 +0900

    maptype:pull하고수정예정

--- client/src/component/article/MapType.js
+++ client/src/component/article/MapType.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import styled from 'styled-components'
 import { useDispatch, useSelector } from 'react-redux'
 import { postingmapMode } from '../../actions'
+import { changeMarkerSize } from '../../servertest/mapResource'
 import { Gear } from './ColorMap'
 
 const MapSection = styled.div`
@@ -26,11 +27,7 @@ function MapType({post}) {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    console.log(data)
-    navigator.geolocation.getCurrentPosition(position => {
-      // console.log(position.coords.latitude)
-      // console.log(position.coords.longitude)
-    })
+    // console.log(data)
     const container = document.getElementById('map')
     const options = {
       center: new kakao.maps.LatLng(data.lng, data.lat),
@@ -40,7 +37,7 @@ function MapType({post}) {
     const map = new kakao.maps.Map(container, options)
 
     const getEmotion = () => {
-      switch(data.emotions) {
+      switch(data.emotions[1]) {
         case 1:
           return 'https://cdn.discordapp.com/attachments/929022343689420871/929022390179094558/2022-01-07_11.32.39.png'
         case 2: 
@@ -51,8 +48,6 @@ function MapType({post}) {
           return 'https://cdn.discordapp.com/attachments/929022343689420871/929022390900518952/2022-01-07_11.33.04.png'
         case 5:
           return 'https://cdn.discordapp.com/attachments/929022343689420871/929022390674010112/2022-01-07_11.32.58.png'
-        default:
-          return 'https://cdn.discordapp.com/attachments/929022343689420871/929022390179094558/2022-01-07_11.32.39.png'
       }
       
     }
@@ -71,7 +66,6 @@ function MapType({post}) {
         image: markerImage,
       })
       marker.setMap(map)
-      marker.setDraggable(true)
     }
 
     getMarkerImage()
@@ -85,8 +79,14 @@ function MapType({post}) {
     // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
     var zoomControl = new kakao.maps.ZoomControl()
     map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT)
-  }, [])
 
+    kakao.maps.event.addListener(map, 'zoom_changed', () => {
+      changeMarkerSize(map, [data.marker], [data])
+      console.log('세번째 인자',data)
+      }
+    )    
+  }, [])
+  // console.log('data가 뭐니',data)
   return (
     <MapSection id='map'>
        {/* <Gear />
