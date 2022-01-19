98f29c9d46f9a1edb44e8acd77ee3f4f87960490
hyeyoungkim <velvetredvelvet91@gmail.com>
2022-01-19 14:34:00 +0900

    PM 02:23 랜딩페이지 작성중

--- client/src/component/Article.js
+++ client/src/component/Article.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux'
 import CaseOfArticleTrue from './article/CaseOfArticleTrue'
 import CaseOfArticleFalse from './article/CaseOfArticleFalse'
 import './article.css'
+import LandingPage from '../pages/LandingPage'
 
 function Article() {
   const state = useSelector(state => state.loginReducer)
