4df04635358fd2b1be0a4729cb45174a1a673bda
dh <jres1007@gmail.com>
2022-01-23 08:58:28 +0900

    env modified

--- server/src/routes/users/post/cookieOptions.ts
+++ server/src/routes/users/post/cookieOptions.ts
@@ -1,18 +1,19 @@
 import { CookieOptions } from 'express'
+require('dotenv').config()
 
 export const cookieOptions: CookieOptions = {
   expires: new Date(Date.now() + 60 * 60000),
-  domain: 'localhost',
+  domain: process.env.COOKIE_OPT_DOMAIN,
   httpOnly: true,
   path: '/',
-  secure: false,
+  secure: process.env.COOKIE_OPT_SECURE,
   sameSite: 'lax',
 }
 
 export const clearCookieOptions: CookieOptions = {
-  domain: 'localhost',
+  domain: process.env.COOKIE_OPT_DOMAIN,
   httpOnly: true,
   path: '/',
-  secure: false,
+  secure: process.env.COOKIE_OPT_SECURE,
   sameSite: 'lax',
 }
